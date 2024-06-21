ac0d36e823a288cd400d30a189651de30b724b7f
Kartikeya Tripathi <96724863+ktwillcode@users.noreply.github.com>
2024-06-21 12:10:57 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const responses = {
         "experience": `My experience includes:
-        - **SRIP Research Scholar - IITRAM, India** (May 2023 - Jul 2023): Implemented RL & ML on an Energy-Efficient Biplane Quadcopter.
         - **Arrhythmia Detection (PPG)** (Feb 2024 - Present): Working on a Vision Transformer model for PPG signals.
+        - **SRIP Research Scholar - IITRAM, India** (May 2023 - Jul 2023): Implemented RL & ML on an Energy-Efficient Biplane Quadcopter.
         - **Contributor - Hacktoberfest** (Oct 2022): Contributed to multiple organizations in web development and documentation.`,
         "projects": `Here are some of my projects:
         - **Seattle Weather Prediction**: Conducted weather prediction for Seattle using historical data.
@@ -15,16 +15,16 @@ document.addEventListener('DOMContentLoaded', () => {
         - **Indoor Localization Scheme**: Improved indoor localization techniques.
         - **Studek**: Working on a website to help students find scholarships and internships.`,
         "skills": `My skills include:
-        - Python, Data Analysis, Machine Learning, Pandas, Scikit-learn, TensorFlow, PyTorch
+        - Python, Data Analysis, Machine Learning, Natural Language Processing (NLP): NLTK, spaCy, Transformers., Pandas, Scikit-learn, TensorFlow, PyTorch
         - Signal Processing, Reinforcement Learning, MATLAB, Django, SQL, HTML/CSS, Git, Linux, LaTeX`,
         "achievements": `Some of my achievements are:
+        - Won URA-1 (only project to win this)
+        - Top 5% of ML Engineers certified by pro5.ai
         - AWS Machine Learning Foundation 2022 - Udacity & AWS
         - CS 229 & CS 234 - Stanford
         - Volunteered at a State-Level Government Hackathon
         - Founder - Jan Hopes Foundation
-        - MEXT Scholarship (Interview Cleared)
-        - Won URA-1 (only project to win this)
-        - Top 5% of ML Engineers certified by pro5.ai`,
+        - MEXT Scholarship (Interview Cleared)`,
         "contact": `You can contact me at:
         - **Email**: kartikeya.kiran24@gmail.com
         - **Phone**: +91 9759221811
@@ -43,6 +43,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    window.addPrompt = function(prompt) {
+        appendMessage('user', prompt);
+        handleUserInput(prompt);
+    };
 
     function appendMessage(sender, message) {
         const messageElement = document.createElement('div');
